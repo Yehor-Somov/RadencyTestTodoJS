275d84edcadd2e08b5f915ae40832c7da7f834aa
YehorSomov <yehor.somov@gmail.com>
2022-09-12 22:43:40 +0300

    realized additing new notes

--- scripts/script.js
+++ scripts/script.js
@@ -6,4 +6,61 @@ window.onload = () => {
 
 function initialize() {
   updateNotesList(notes);
+
+  document
+    .querySelector(".create-note-button")
+    .addEventListener("click", addNote);
+}
+
+function addNote() {
+  const categorySelector = document.querySelector(".input-category");
+  const noteNameInput = document.querySelector(".input-note-name");
+  const noteContentInput = document.querySelector(".input-content");
+
+  let newNote = {
+    icon: getCategoryIcon(categorySelector.value),
+    name: noteNameInput.value,
+    created: dateFormatter(new Date()),
+    category: categorySelector.value,
+    state: "active",
+    content: noteContentInput.value,
+    dates: parsingContentDates(noteContentInput.value),
+  };
+
+  notes.push(newNote);
+  updateNotesList(notes);
+  closeModal();
+}
+
+function dateFormatter(date) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+}
+
+function parsingContentDates(content) {
+  const datesArray = content.match(/\d{2}([\/.-])\d{2}\1\d{4}/g);
+  return datesArray !== null ? datesArray : "";
+}
+
+function getCategoryIcon(category) {
+  if (category === "Task") {
+    return "fa-thumbtack";
+  } else if (category === "Random Thought") {
+    return "fa-brain";
+  } else if (category === "Idea") {
+    return "fa-lightbulb";
+  }
 }
