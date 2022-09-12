67ca311009d098167a35f7baa9b7c0f0f1220acb
YehorSomov <yehor.somov@gmail.com>
2022-09-12 22:33:20 +0300

    add modal window for new/edit notes

--- scripts/layout.js
+++ scripts/layout.js
@@ -1,3 +1,7 @@
+const modalWindow = document.querySelector(".modal");
+const addNoteButton = document.querySelector(".add-note-button");
+const closeModalButton = document.querySelector(".close-modal-button");
+
 function updateNotesList(notes) {
   const notesList = document.querySelector(".notes-list");
   notesList.innerHTML = "";
@@ -10,11 +14,11 @@ function updateNotesList(notes) {
     noteElement.innerHTML = `<div class="note-icon">
                                     <i class="fa-solid ${note.icon} fa-xl"></i>
                                 </div>
-                                <p class="note-name">${note.name}</p>
-                                <p class="note-date-created">${note.created}</p>
-                                <p class="note-category">${note.category}</p>
-                                <p class="note-content">${note.content}</p>
-                                <p class="note-context-dates">${note.dates}</p>
+                                <p class="note-name note-list-info-field">${note.name}</p>
+                                <p class="note-date-created note-list-info-field">${note.created}</p>
+                                <p class="note-category note-list-info-field">${note.category}</p>
+                                <p class="note-content note-list-info-field">${note.content}</p>
+                                <p class="note-context-dates note-list-info-field">${note.dates}</p>
                                 <div class="note-toolbar">
                                     <div class="edit-button note-btn" data-action="edit">
                                         <i class="fa-solid fa-pen"></i>
@@ -37,3 +41,21 @@ document.querySelector(".notes-types").addEventListener("change", () => {
   const option = document.querySelector(".notes-types").value;
   document.querySelector(".notes-list").dataset.option = option;
 });
+
+addNoteButton.addEventListener("click", () => {
+  document.querySelector(".modal-content").dataset.modalType = "new-note";
+  openModal();
+});
+
+closeModalButton.addEventListener("click", closeModal);
+
+function openModal() {
+  modalWindow.classList.add("modal-active");
+}
+
+function closeModal() {
+  modalWindow.classList.remove("modal-active");
+  document.querySelector(".input-category").value = "Task";
+  document.querySelector(".input-note-name").value = "";
+  document.querySelector(".input-content").value = "";
+}
